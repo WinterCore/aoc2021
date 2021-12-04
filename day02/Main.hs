c07a1e37bafa1a19bf08bac093148b95b5d1551e
WinterCore <hogobbl@gmail.com>
2021-12-04 13:04:03 +0200

    Add day 2 / part 2

--- day02/Main.hs
+++ day02/Main.hs
@@ -20,6 +20,7 @@ main = do
     contents <- readFile "input"
     let commands = mapMaybe strToCommand (lines contents)
     putStrLn $ "Part 1: " ++ solve1 commands
+    putStrLn $ "Part 2: " ++ solve2 commands
     return ()
 
 solve1 :: [Command] -> String
@@ -33,3 +34,12 @@ solve1 cmds = show $ verticalPos * horizontalPos
                                        c        -> 0
         verticalPos = sum . map getVerticalValue $ cmds
         horizontalPos = sum . map getHorizontalValue $ cmds
+
+solve2 :: [Command] -> String
+solve2 cmds = let (a, b, c) = foldl execute (0, 0, 0) cmds in show $ b * c
+    where
+        -- (Int, Int, Int) represents (aim, depth, horizontal position)
+        execute :: (Int, Int, Int) -> Command -> (Int, Int, Int)
+        execute (a, b, c) (Up n)      = (a - n, b, c)
+        execute (a, b, c) (Down n)    = (a + n, b, c)
+        execute (a, b, c) (Forward n) = (a, b + (n * a), c + n)
