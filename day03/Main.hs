ee439f36230dedf85b1db02d745ff69f34092cb4
WinterCore <hogobbl@gmail.com>
2021-12-05 08:47:41 +0200

    Add day 3 / part 2

--- day03/Main.hs
+++ day03/Main.hs
@@ -1,14 +1,15 @@
 module Main where
 
-import Data.List (transpose)
 import Data.Bits
+import Data.List (transpose)
+import Data.Char (digitToInt)
 
 main :: IO ()
 main = do
     contents <- readFile "input"
     let binaryNums = lines contents
     putStrLn $ "Part 1: " ++ solve1 binaryNums
-    print $ binToInt [1, 0, 1, 1, 0]
+    putStrLn $ "Part 2: " ++ solve2 binaryNums
     return ()
 
 binToInt :: [Int] -> Int
@@ -17,14 +18,52 @@ binToInt = foldl (\r x -> shift r 1 .|. x) 0
 binToBitStates :: [Char] -> (Int, Int)
 binToBitStates bin = (on bin, off bin)
     where
-        on = length . filter (== '0')
-        off = length . filter (== '1')
+        on = length . filter (== '1')
+        off = length . filter (== '0')
+
+toProminent :: (Int, Int) -> (Int, Int)
+toProminent (a, b) = if a > b then (1, 0) else (0, 1)
+
+mapSnd :: (b -> c) -> (a, b) -> (a, c)
+mapSnd f (a, b) = (a, f b)
 
 solve1 :: [String] -> String
 solve1 binaryNums = show $ gamma * epsilon
     where
         columns = transpose binaryNums
         states = map binToBitStates columns
-        toRatio (a, b) = if a > b then (1, 0) else (0, 1)
-        gamma = binToInt . map (fst . toRatio) $ states
-        epsilon = binToInt . map (snd . toRatio) $ states
+        gamma = binToInt . map (fst . toProminent) $ states
+        epsilon = binToInt . map (snd . toProminent) $ states
+
+
+shake :: ((Int, Int) -> Int) -> Int -> [String] -> [String]
+shake predicate i bins =
+    if i == bitLength || length bins == 1 then bins
+    else shake predicate (i + 1)
+        . filterBitAtIndex (head . show $ prominent) i
+        $ bins
+    where
+        prominent = predicate
+                    . binToBitStates
+                    . (!! i)
+                    . transpose
+                    $ bins
+        filterBitAtIndex b i = filter ((== b) . (!! i))
+        bitLength = length . head $ bins
+
+solve2 :: [String] -> String
+solve2 bins = show . uncurry (*) $ (oxygen bins, co2 bins)
+    where
+        oxygen = getRating oxygenPredicate
+        co2 = getRating co2Predicate
+        getRating predicate = binToInt
+                              . map digitToInt
+                              . head
+                              . shake predicate 0
+        oxygenPredicate (a, b)
+            | a == b = 1
+            | otherwise = fst . toProminent $ (a, b)
+        co2Predicate (a, b)
+            | a == b = 0
+            | otherwise = snd . toProminent $ (a, b)
+        
