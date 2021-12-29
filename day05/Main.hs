02ee05cbd70301cea0c24dcc6b4f5fc014a33564
WinterCore <hogobbl@gmail.com>
2021-12-29 03:30:32 +0200

    Add day 5 / part 2

--- day05/Main.hs
+++ day05/Main.hs
@@ -27,19 +27,34 @@ main = do
     let vents = map parseVentLine . lines $ contents
 
     putStrLn $ "Part 1: " ++ solve1 vents
+    putStrLn $ "Part 2: " ++ solve2 vents
 
     return ()
 
 range :: Int -> Int -> [Int]
 range a b = if a < b then [a..b] else [a, (a - 1)..b]
 
-ventLineToPoints :: VentLine -> [Point]
-ventLineToPoints ((x1, y1), (x2, y2))
+ventLineToPointsBasic :: VentLine -> [Point]
+ventLineToPointsBasic ((x1, y1), (x2, y2))
     | x1 == x2  = zip (repeat x1) (range y1 y2)
     | y1 == y2  = zip (range x1 x2) (repeat y1)
-    | otherwise = [] -- We don't handle diagonals for now
+    | otherwise = []
+
+-- The difference between this function and the one above
+-- is that this one processes diagonal lines
+ventLineToPointsAdvanced :: VentLine -> [Point]
+ventLineToPointsAdvanced ((x1, y1), (x2, y2))
+    | x1 == x2  = zip (repeat x1) (range y1 y2)
+    | y1 == y2  = zip (range x1 x2) (repeat y1)
+    | otherwise = zip (range x1 x2) (range y1 y2)
 
 solve1 :: [VentLine] -> String
 solve1 vls = show . length . M.filter (>= 2) $ graphMap
-    where points   = concatMap ventLineToPoints vls
+    where points   = concatMap ventLineToPointsBasic vls
           graphMap = M.fromListWith (+) . map (, 1) $ points
+
+solve2 :: [VentLine] -> String
+solve2 vls =  show . length . M.filter (>= 2) $ graphMap
+    where points   = concatMap ventLineToPointsAdvanced vls
+          graphMap = M.fromListWith (+) . map (, 1) $ points
+
